999718f06e55da1b4caf1ac01bc7a0710a02d98b
Sweetbread <sweetbread@coders-squad.com>
2024-04-24 17:14:59 +0300

    [style]: Change mako and wofi style

---
+++ home-manager/modules/wms/mako.nix
@@ -0,0 +1,12 @@
+{
+  xdg.configFile."mako/config".text = ''
+    background-color=#303446
+    text-color=#c6d0f5
+    border-color=#8caaee
+    progress-color=over #414559
+    default-timeout=5000
+
+    [urgency=high]
+    border-color=#ef9f76
+  '';
+}
