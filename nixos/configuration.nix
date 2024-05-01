5f2c0516efe0fdea7b024cfd717d35f94dd69608
Sweetbread <sweetbread@coders-squad.com>
2024-05-01 15:39:14 +0300

    [system]: Nvidia

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,4 @@
-{
+{ config, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./packages.nix
@@ -18,4 +18,27 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
 
   system.stateVersion = "23.05"; # Don't change it bro
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      sync.enable = true;
+		  intelBusId = "PCI:0:2:0";
+		  nvidiaBusId = "PCI:1:0:0";
+	  };
+  };
 }
