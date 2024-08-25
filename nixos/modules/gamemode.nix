8cf14d59c88d2307771e7ca46fde2cfeb9ba6dd4
Sweetbread <sweetbread@coders-squad.com>
2024-08-26 01:01:46 +0300

    system: Add Game module

---
+++ nixos/modules/gamemode.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup
+    bottles
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+}
