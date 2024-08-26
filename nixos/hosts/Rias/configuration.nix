238d9480b43ec7645493270a7d85d52965db9156
Sweetbread <sweetbread@coders-squad.com>
2024-08-26 12:41:53 +0300

    system: Update

--- nixos/hosts/Rias/configuration.nix
+++ nixos/hosts/Rias/configuration.nix
@@ -16,10 +16,9 @@
 
   system.stateVersion = "23.05";
 
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [nvidia-vaapi-driver intel-media-driver];
     extraPackages32 = with pkgs.pkgsi686Linux; [nvidia-vaapi-driver intel-media-driver];
   };
