718b1952262987f525a3fee4b7055d23ec7d10dc
Sweetbread <sweetbread@coders-squad.com>
2024-09-10 13:46:44 +0300

    pkgs: Add Ayugram

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     sops-nix.url = "github:Mic92/sops-nix";
     stylix.url = "github:danth/stylix";
+    ayugram-desktop.url = "github:kaeeraa/ayugram-desktop/release?submodules=1";
 
     home-manager = {
       url = "github:nix-community/home-manager";
