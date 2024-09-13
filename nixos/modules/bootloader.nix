70ef35b9caac2fcd4970ec3d3807d3611676550b
Sweetbread <sweetbread@coders-squad.com>
2024-09-13 18:36:09 +0300

    system: Boot animation

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,6 +1,33 @@
-{
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.kernelModules = [ "nvidia" ];
-  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ]; 
+{ pkgs, ... }: {
+  boot = {
+    loader = {
+      timeout = 3;
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    initrd = {
+      verbose = false;
+      systemd.enable = true;
+    };
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      # "loglevel=3"
+      # "rd.systemd.show_status=false"
+      # "rd.udev.log_level=3"
+      "udev.log_priority=0"
+    ];
+    plymouth = {
+      enable = true;
+      theme = "black_hud";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "black_hud" ];
+        })
+      ];
+    };
+  };
 }
