625c6a283dfc6121d0d81b494a9c2e27b82ccf9e
Sweetbread <sweetbread@coders-squad.com>
2024-09-16 11:36:27 +0300

    style: Change wofi style

--- home-manager/users/sweetbread/modules/wms/wofi.nix
+++ home-manager/users/sweetbread/modules/wms/wofi.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
-  xdg.configFile."wofi/style.css".source = pkgs.requireFile {
+  xdg.configFile."wofi/style.css".source = pkgs.fetchurl {
     name = "style.css";
-    url = "https://raw.githubusercontent.com/PaulGuerre/arch_dotfiles/main/wofi/style.css";
-    sha256 = "16lr4j0nn6kpggr4wdz9s0jymqmn05vm7i4vsdslzp5vlarcv03j";
+    url = "https://github.com/joao-vitor-sr/wofi-themes-collection/raw/main/themes/nord.css";
+    sha256 = "sha256-rMDtE7Q0hmqd9LD+Ur/QR6cMan9ev6e9IyzpwY367c0=";
   };
 }
