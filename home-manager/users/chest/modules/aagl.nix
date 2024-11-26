974df4afe1b4f04d382ff3964b69c591012a4cf8
Sweetbread <sweetbread@coders-squad.com>
2024-11-26 22:01:37 +0300

    pkgs: Update aagl

--- home-manager/users/chest/modules/aagl.nix
+++ home-manager/users/chest/modules/aagl.nix
@@ -2,7 +2,7 @@ let
   aagl-gtk-on-nix = import (
     builtins.fetchTarball {
       url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-      sha256 = "172nwbkix2wx3sq7z1mchm1awmy5klvcmsn54jylx85nypx67wzx";
+      sha256 = "14fnbcvmr1b8jkkjfbyarljwawgh3as9skdyvyc10b1nqh0641h0";
     }
   );
 in
