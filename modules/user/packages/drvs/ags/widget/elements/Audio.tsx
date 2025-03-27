3acbf43b4e51b4388b7aa1db4428c90db2cd23e4
Sweetbread <risdeveau@codrs.ru>
2025-03-27 23:03:55 +0300

    feat: add pulsemixer

--- modules/user/packages/drvs/ags/widget/elements/Audio.tsx
+++ modules/user/packages/drvs/ags/widget/elements/Audio.tsx
@@ -1,11 +1,16 @@
 import { bind } from "astal"
+import { execAsync } from "astal/process"
 import Wp from "gi://AstalWp"
 
 export default function Audio() {
     const speaker = Wp.get_default()?.audio.defaultSpeaker!
 
-    return <box className="AudioSlider">
-        <icon icon={bind(speaker, "volumeIcon")} />
-        <label label={bind(speaker, "volume").as(v => `${Math.floor(v*100)}%`)} />
-    </box>
+    return <button
+        className="AudioSlider"
+        onClicked={() => { execAsync("ghostty --title=pulsemixer -e pulsemixer") }}>
+        <box>
+            <icon icon={bind(speaker, "volumeIcon")} />
+            <label label={bind(speaker, "volume").as(v => `${Math.floor(v*100)}%`)} />
+        </box>
+    </button>
 }
