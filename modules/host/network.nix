e16d4fa3b401221ed8f92e649d474abcbb1618f0
Sweetbread <risdeveau@codrs.ru>
2025-08-15 13:36:14 +0300

    fixup! system: update

--- modules/host/network.nix
+++ modules/host/network.nix
@@ -2,14 +2,14 @@
   networking = {
     enableIPv6 = true;
     useDHCP = false;
-    nameservers = [ "185.246.223.236" "1.1.1.1" ];
+    nameservers = [ "64.188.64.176" "1.1.1.1" ];
 
     dhcpcd.extraConfig = "nohook resolv.conf";
 
     networkmanager = {
       enable = true;
       dns = "none";
-      insertNameservers = [ "185.246.223.236" "1.1.1.1" ];
+      insertNameservers = [ "64.188.64.176" "1.1.1.1" ];
     };
   };
 }
