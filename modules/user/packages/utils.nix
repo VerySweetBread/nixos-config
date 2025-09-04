0b7bf06f58ecf9b02bb6186f43cb2227583c88ff
Sweetbread <risdeveau@codrs.ru>
2025-09-04 19:00:49 +0300

    fix: set imv as a image viewer

--- modules/user/packages/utils.nix
+++ modules/user/packages/utils.nix
@@ -22,4 +22,28 @@
     imagemagick
     wl-clipboard
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "image/bmp"                = "imv.desktop";
+      "image/gif"                = "imv.desktop";
+      "image/jpeg"               = "imv.desktop";
+      "image/jpg"                = "imv.desktop";
+      "image/pjpeg"              = "imv.desktop";
+      "image/png"                = "imv.desktop";
+      "image/tiff"               = "imv.desktop";
+      "image/x-bmp"              = "imv.desktop";
+      "image/x-pcx"              = "imv.desktop";
+      "image/x-png"              = "imv.desktop";
+      "image/x-portable-anymap"  = "imv.desktop";
+      "image/x-portable-bitmap"  = "imv.desktop";
+      "image/x-portable-graymap" = "imv.desktop";
+      "image/x-portable-pixmap"  = "imv.desktop";
+      "image/x-tga"              = "imv.desktop";
+      "image/x-xbitmap"          = "imv.desktop";
+      "image/heif"               = "imv.desktop";
+      "image/avif"               = "imv.desktop";
+    };
+  };
 }
