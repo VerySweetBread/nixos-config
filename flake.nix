9281b78cd99a2529f62f3a1697cc8aa11ae3db62
Sweetbread <risdeveau@codrs.ru>
2025-12-14 18:28:47 +0300

    yazi: add some stuff

--- flake.nix
+++ flake.nix
@@ -7,12 +7,14 @@
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://ezkea.cachix.org"
+      "https://yazi.cachix.org"
     ];
     extra-trusted-public-keys = [
       "main:kpwMe+9BsGJ/IUb7i3iadaV38y5/Yuqoct0mf7wI9ds="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
   };
 
