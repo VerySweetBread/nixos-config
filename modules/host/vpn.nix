9d30ab07fa7f85ca7f9d1d4f7d059eed7fcbbc77
Sweetbread <risdeveau@lair.moe>
2026-04-03 22:45:28 +0300

    vpn: change engine

--- modules/host/vpn.nix
+++ modules/host/vpn.nix
@@ -11,6 +11,7 @@
     wantedBy = [ "multi-user.target" ];
     environment = {
       V2RAYA_LOG_FILE = "/var/log/v2raya/v2raya.log";
+      V2RAYA_V2RAY_BIN = pkgs.lib.getExe pkgs.xray;
       V2RAY_LOCATION_ASSET = "/etc/v2raya";
       XRAY_LOCATION_ASSET = "/etc/v2raya";
     };
