296312e960fce0072ce8c156e76a97192b1f0a40
Sweetbread <risdeveau@lair.moe>
2026-04-06 15:12:22 +0300

    fixup! Update system

--- user/sweetbread/modules/style.nix
+++ user/sweetbread/modules/style.nix
@@ -142,8 +142,11 @@ in {
   };
 
   gtk = {
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
+    gtk4 = {
+      theme = null;
+      extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
     };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
